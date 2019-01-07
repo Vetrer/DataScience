27bc118efa97959fa232a6f7b9575b8c2b693da1
Marcos Vinicius Alves de Souza <36246247+Vetrer@users.noreply.github.com>
2019-01-06 22:28:50 -0200

    Update Siraj 3.py

--- Siraj 3.py
+++ Siraj 3.py
@@ -3,32 +3,32 @@ from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
 
 data = fetch_movielens(min_rating=4.0)
-#print training ans testing data
+#print treino de teste de dados
 print(repr(data['train']))
 print(repr(data['test']))
 
-#create model
+#criação do modelo
 model = LightFm(loss='warp')
-#train model
+#treinar modelo
 model.fit(data['train'], epachs=30, num_threads=2)
 
 def sample_recommendation(model, data, user_ids):
 
-    #number of users and movies in training data
+    #numero dos usuario e itens
     n_users, n_items = data['train'].shape
 
-    #generate recommendations for each user we input
+    #gerar recomendações
     for user_id in user_ids:
 
-        #movies they already like
+        #modelo que preve que todos vão gostar
         known_positives = data['item_labels'][data['train'].tocsr()[user_id].indices]
 
-        #movies our model predicts they will like
+       #modelo que preve que eles vão gostar
         scores = model.predict(user_id, np.arange(n_items))
         #rank them in order of most liked to least
         top_items = data['item_labels'][np.argsort(-scores)]
 
-        #print out the results
+        #print os resuktas
         print("User %s" % user_id)
         print("    Known positives:")
 
